dfa711f91b8f2c196cfcc7098abb8a64c9dec018
GAZA16 <aa.ale.are@gmail.com>
2025-09-30 22:48:26 -0600

    no message

---
+++ src/components/Formulario.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "FormularioRegistro", // <- nombre de componente multi-word
+  data() {
+    return {
+      nombre: "",
+      edad: "",
+      correo: "",
+      enviado: false,
+    };
+  },
+  methods: {
+    enviarFormulario() {
+      this.enviado = true;
+    },
+  },
+};
+</script>
